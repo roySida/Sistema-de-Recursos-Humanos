bd341891fd00e9756ebdf20067f3e67c31b721e2
EdgarM10pro <etramos.qro@icloud.com>
2023-04-03 20:36:13 -0600

    Actividad 7

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const morgan = require('morgan')
 const express = require('express')
 const app = express()
-const {pokemon} = require('./pokedex.json')
+const pokemon = require('./routes/pokemon')
 
+app.use(morgan('dev'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -9,35 +11,7 @@ app.get("/", (req, res, next) => {
     return res.status(200).send("Bienvenido al Pokedex")
 })
 
-app.post("/pokemon", (req, res, next) => {
-    //console.log(req.body)
-    return res.status(200).send(req.body)
-})
-
-app.get("/pokemon/:id([0-9]{1,3})", (req, res, next) => {
-    const id = req.params.id - 1
-    (id >= 0 && id <= 150) ? 
-    res.status(200).send(pokemon[id]) :
-    res.status(404).send("Pokemon no encontrado")
-})
-
-app.get("/pokemon/:name([A-Za-z]+)", (req, res, next) => {
-    const name = req.params.name
-    const pk = pokemon.filter((p) => {
-        return (p.name.toUpperCase() == name.toUpperCase()) ? p : null
-    })
-
-    if(pk.length > 0) {
-        res.status(200).send(pk)
-    } else {
-        res.status(404).send("Pokemon no encontrado")
-    }
-    
-})
-
-app.get("/pokemon", (req, res, next) => {
-    return res.status(200).send(pokemon)
-})
+app.use("/pokemon", pokemon)
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("server is running...")
---
+++ routes/pokemon.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const pokemon = express.Router()
+const db = require('../config/database')
+
+pokemon.post("/", (req, res, next) => {
+    //console.log(req.body)
+    return res.status(200).send(req.body)
+})
+
+pokemon.get("/", async (req, res, next) => {
+    const pkmn = await db.query("SELECT * FROM pokemon")
+    return res.status(200).json(pkmn)
+})
+
+pokemon.get("/:id([0-9]{1,3})", (req, res, next) => {
+    const id = req.params.id - 1
+    return (id >= 0 && id <= 150) ? res.status(200).send(pk[id]) : res.status(404).send("Pokemon no encontrado")
+})
+
+pokemon.get("/:name([A-Za-z]+)", (req, res, next) => {
+    const name = req.params.name
+    const pkmn = pk.filter((p) => {
+        return (p.name.toUpperCase() == name.toUpperCase()) ? p : null
+    })
+
+    return (pkmn.length > 0) ? res.status(200).send(pkmn) : res.status(404).send("Pokemon no encontrado")
+})
+
+module.exports = pokemon
+
