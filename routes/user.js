862ec22516e31147a405cbb533a4ecb314fbe1db
EdgarM10pro <etramos.qro@icloud.com>
2023-05-05 16:19:26 -0600

    Actividad 12

--- routes/user.js
+++ routes/user.js
@@ -21,7 +21,7 @@ user.post("/login", async (req, res, next) => {
     const{user_mail, user_password} = req.body
     const query = `SELECT * FROM user WHERE user_mail = '${user_mail}' AND user_password = '${user_password}'`
     const rows = await db.query(query)
-
+    
     if(user_mail && user_password){
         if(rows.length == 1){
             const token = jwt.sign({
