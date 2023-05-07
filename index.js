69be27de22322d75e252275f57376b7b2a05c0b5
EdgarM10pro <etramos.qro@icloud.com>
2023-05-07 17:51:11 -0600

    Actividad 13

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
+//Dependencies 
 const morgan = require('morgan')
 const express = require('express')
 const app = express()
+//Routers
 const pokemon = require('./routes/pokemon')
 const user = require('./routes/user')
+//Middleware
+const auth = require('./middleware/auth')
+const notFound = require('./middleware/notFound')
+const bienvenida = require('./middleware/bienvenida')
 
 app.use(morgan('dev'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res, next) => {
-    return res.status(200).json({code: 1, message: "Bienvenido al Pokedex"})
-})
-
-app.use("/pokemon", pokemon)
-
+app.get("/", bienvenida)
 app.use("/user", user)
-
-app.use((req, res, next) => {
-    return res.status(404).json({code: 404, message: "URL no encontrada"})
-})
+app.use(auth)
+app.use("/pokemon", pokemon)
+app.use(notFound)
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("server is running...")
