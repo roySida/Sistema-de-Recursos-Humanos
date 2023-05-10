6031d83a28fa5fe0df56ed6796587991c21b85c6
EdgarM10pro <etramos.qro@icloud.com>
2023-05-10 17:10:54 -0600

    Actividad 14

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const user = require('./routes/user')
 const auth = require('./middleware/auth')
 const notFound = require('./middleware/notFound')
 const bienvenida = require('./middleware/bienvenida')
+const cors = require("./middleware/cors")
 
+app.use(cors)
 app.use(morgan('dev'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
