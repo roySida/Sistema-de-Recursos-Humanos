4dc890ec668af11ea73dde323742d98dd1f27693
YYNOVELL <130712227+YYNOVELL@users.noreply.github.com>
2023-06-01 19:50:27 -0600

    Add files via upload

---
+++ home.php
@@ -0,0 +1,42 @@
+ <!-- Header-->
+ <header class="bg-dark py-5" id="main-header">
+    <div class="container px-4 px-lg-5 my-5">
+        <div class="text-center text-white">
+            <h1 class="display-4 fw-bolder"><?php echo $_settings->info('name') ?></h1>
+            <p class="lead fw-normal text-white-50 mb-0">
+                <button class="btn btn-sm btn-primary" type="button" id="create_appointment">Agendar Cita Ahora</button>
+                <button class="btn btn-sm btn-primary" type="button" id="create_registro">Registrarse</button>
+            </p>
+        </div>
+    </div>
+</header>
+<!-- Section-->
+<?php 
+$sched_arr = array();
+$max = 0;
+?>
+<section class="py-5">
+    <div class="container px-4 px-lg-5 mt-5">
+        
+    <?php include('about.html') ?>
+        
+    </div>
+</section>
+
+<script>
+    $(function(){
+        $('#create_appointment').click(function(){
+			uni_modal("Datos de la Cita","admin/appointments/manage_appointment.php",'mid-large')
+		})
+    })
+    
+</script>
+
+<script>
+    $(function(){
+        $('#create_registro').click(function(){
+			uni_modal("Ingrese sus datos","admin/appointments/registrarse.php",'mid-large')
+		})
+    })
+    
+</script>
